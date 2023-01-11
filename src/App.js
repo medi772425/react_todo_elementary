3d94b179e20f3e06cf9d48310272a40e34c1b6aa
medi <medi772425@gmail.com>
2023-01-12 06:29:11 +0900

    追加ボタン押下時、ToDoを配列へ追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-// import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [todos, setTodos] = useState(['ToDo1', 'ToDo2']);
+
+  const addTodo = () => {
+    const new_todo = document.querySelector('#text_new_todo').value;
+
+    setTodos((prevTodos) => [...prevTodos, new_todo]);
+  };
+
   return (
     <>
       <h1>ToDo</h1>
       <div>
+        <input type="text" id="text_new_todo" />
+        <button onClick={addTodo}>追加</button>
         <ul>
           <li>ToDo1</li>
           <li>ToDo2</li>
