b3afd4bba1e3df931183cfdb1f588f310e9cf414
medi <medi772425@gmail.com>
2023-01-17 06:17:05 +0900

    ステータスselectboxのselectedをvalueに変更

--- src/components/StateSelect.js
+++ src/components/StateSelect.js
@@ -6,13 +6,6 @@ const StateSelect = ({ onChange, status, id, isEmptyOption }) => {
     <select id={id} value={status} onChange={onChange}>
       {isEmptyOption && <option value=""></option>}
       {statuses.map((statusObj) => {
-        let selected = '';
-
-        // if (statusObj.value === status) {
-        //   selected = 'selected';
-        // }
-        // selected={selected}
-
         return (
           <option key={statusObj.value} value={statusObj.value}>
             {statusObj.text}
